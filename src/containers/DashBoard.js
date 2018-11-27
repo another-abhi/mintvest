5f46a793b549ff4f02675c0a6defeae1704af597
Abhishek Suresh <AbhishekS@namindia.com>
2018-11-27 15:50:48 +0530

    mintvest v0.1

--- src/containers/DashBoard.js
+++ src/containers/DashBoard.js
@@ -7,6 +7,7 @@ import {
   Tooltip
 } from "react-bootstrap";
 import "./DashBoard.css";
+import finapp from "../static/images/finapp.jpg";
 
 export default class DashBoard extends Component {
   constructor(props) {
@@ -16,6 +17,7 @@ export default class DashBoard extends Component {
       show: true
     };
   }
+
   handleClose = () => {
     this.setState({ show: false });
   };
@@ -28,6 +30,9 @@ export default class DashBoard extends Component {
   };
 
   render() {
+    const divStyle = {
+      backgroundImage: finapp
+    };
     const popover = (
       <Popover id="modal-popover" title="popover">
         very popover. such engagement
@@ -35,64 +40,69 @@ export default class DashBoard extends Component {
     );
     const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
     return (
-      <div className="Dashboard">
-        <Modal
-          show={this.state.show}
-          onHide={this.handleClose}
-          bsSize="small"
-          bsClass="custom-modal"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-            <Button onClick={this.handleGo}>GO!!!!</Button>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Text in a modal</h4>
+      <div style={divStyle}>
+        <div className="Dashboard">
+          <Modal
+            show={this.state.show}
+            onHide={this.handleClose}
+            bsSize="small"
+            bsClass="custom-modal"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Invest Now!</Modal.Title>
+              <Button onClick={this.handleGo}>GO!!!!</Button>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>You could be making more</h4>
+              <p>Click to find out how.</p>
+
+              {/* <h4>Popover in a modal</h4>
+              <p>
+                there is a{" "}
+                <OverlayTrigger overlay={popover}>
+                  <a href="#popover">popover</a>
+                </OverlayTrigger>{" "}
+                here
+              </p>
+
+              <h4>Tooltips in a modal</h4>
+              <p>
+                there is a{" "}
+                <OverlayTrigger overlay={tooltip}>
+                  <a href="#tooltip">tooltip</a>
+                </OverlayTrigger>{" "}
+                here
+              </p>
+
+              <hr />
+
+              <h4>Overflowing text to show scroll behavior</h4> */}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+
+          <div className="lander">
+            <h1>Mintvest-FinApp DashBoard</h1>
             <p>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              {console.log(this.props.currentUser)}
+              This will be the dashboard of a logged in user with various
+              placeholders for things they see. On this page each user will get
+              a push ad kind of notification.
             </p>
-
-            <h4>Popover in a modal</h4>
-            <p>
-              there is a{" "}
-              <OverlayTrigger overlay={popover}>
-                <a href="#popover">popover</a>
-              </OverlayTrigger>{" "}
-              here
-            </p>
-
-            <h4>Tooltips in a modal</h4>
-            <p>
-              there is a{" "}
-              <OverlayTrigger overlay={tooltip}>
-                <a href="#tooltip">tooltip</a>
-              </OverlayTrigger>{" "}
-              here
-            </p>
-
-            <hr />
-
-            <h4>Overflowing text to show scroll behavior</h4>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleClose}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-
-        <div className="lander">
-          <h1>Mintvest-FinApp DashBoard</h1>
-          <p>
-            {console.log(this.props.currentUser)}
-            This will be the dashboard of a logged in user with various
-            placeholders for things they see. On this page each user will get a
-            push ad kind of notification.
-          </p>
-          <h4>{this.props.currentUser.name}</h4>
-        </div>
-        <div>
-          <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
-            Changed my mind!
-          </Button>
+            <h4>{this.props.currentUser.name}</h4>
+          </div>
+          <div>
+            <Button
+              bsStyle="primary"
+              bsSize="large"
+              onClick={this.handleShow}
+              disabled={this.state.show}
+            >
+              Changed my mind!
+            </Button>
+          </div>
         </div>
       </div>
     );
